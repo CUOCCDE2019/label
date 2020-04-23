3124bc4f63b8d057783571f38329906409f60686
Jmw-Power <41883084+JMWpower@users.noreply.github.com>
2020-04-23 11:49:28 +0800

    Add files via upload

---
+++ custom_test/custom-print-json.js
@@ -0,0 +1,11 @@
+var customPrintJson = {
+    "panels": [{
+        "index": 0,
+        "height": 150,
+        "width": 100,
+        "paperHeader": 0,
+        "paperFooter": 0,
+        "paperNumberDisabled":true,
+        "paperNumberFormat":"paperCount",
+        "printElements": ""
+}]}
